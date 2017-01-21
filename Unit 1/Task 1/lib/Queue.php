e4a7b7cf97e826dcf4362b579fb14343a16c2d92
Anton Lytkin <anton.lytkin18@gmail.com>
2017-01-21 22:02:55 +0300

    Unit 1. Task1. Выполнение задания.

--- Unit 1/Task 1/lib/Queue.php
+++ Unit 1/Task 1/lib/Queue.php
@@ -7,4 +7,21 @@ namespace WS\Education\Unit1\Task1;
 
 class Queue implements Collection {
 
+    private $data;
+
+    public function __construct($type = null) {
+        $this->data = array();
+    }
+
+    public function pop() {
+        return array_shift($this->data);
+    }
+
+    public function size() {
+        return count($this->data);
+    }
+
+    public function push($el) {
+       array_push($this->data, $el);
+    }
 }
